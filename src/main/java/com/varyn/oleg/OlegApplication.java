dd5056c6cd498732b7ffce82d248851f53b1c2af
staskosmos <mr.varyn@mail.ru>
2020-04-28 22:03:00 +0300

    Init REST application project

---
+++ src/main/java/com/varyn/oleg/OlegApplication.java
@@ -0,0 +1,13 @@
+package com.varyn.oleg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/varyn/oleg/OlegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varyn.oleg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
